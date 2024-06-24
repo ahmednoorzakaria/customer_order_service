a4e2c63b2ebae0f537bba00b178d795c1bb53468
Ahmednoor <Ahmednoorzakaria680@gmail.com>
2024-06-24 14:39:31 +0000

    set the database and created database schema/models

--- customer_order_service/api/models.py
+++ customer_order_service/api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Customer(models.Model):
+    name = models.charField(max_length=255)
+    code = models.charField(max_length=100, unique=True)
+    phone_number = models.charField(max_length=15)
+
+
+class Order (models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+    item = models.charField(max_length=255)
+    amount = models.DecimalField(max_digit=10, decimal_places=2)
+    time = models.DateTimeField()
