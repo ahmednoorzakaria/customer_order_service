5e75578e58dd2cd015f3ba4a0f3e6096e93fef77
Ahmednoor <Ahmednoorzakaria680@gmail.com>
2024-06-24 15:02:04 +0000

    created the viewsets and some urls

--- customer_order_service/api/views.py
+++ customer_order_service/api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from .models import Customer, Order
+from .serializers import CustomerSerializer, OrderSerializer
 # Create your views here.
+
+
+class CustomerViewSet(viewsets.ModelViewsSet):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+
+class OrderViewSet (viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
--- customer_order_service/customer_order_service/settings.py
+++ customer_order_service/customer_order_service/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'rest_framework',
+    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
